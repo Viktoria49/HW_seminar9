968d6dae88a017cc1fabdc92fc099097057ac866
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-09-07 14:06:47 +0700

    Tasks

---
+++ Task_66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+Console.WriteLine("Введите число M: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int endNumber = Convert.ToInt32(Console.ReadLine());
+
+
+int SumNaturalNumber (int num1, int num2)
+{
+    if (num1 > num2)
+        return num1 + SumNaturalNumber (num1 - 1, num2);
+    else if (num1 < num2)
+        return num1 + SumNaturalNumber (num1 + 1, num2);
+    else return num1;
+}
+int sum = SumNaturalNumber (firstNumber, endNumber);
+Console.WriteLine ($"Сумма элементов от {firstNumber} до {endNumber} = {sum}");
---
+++ Task_68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите целое положительное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Akk(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return Akk(n - 1, 1);
+    if (n > 0 && m > 0) return Akk(n - 1, Akk(n, m - 1));
+    return Akk(n, m);
+}
+
+int functionAkkerman = Akk(m, n);
+Console.Write($"A({m}, {n}) = {functionAkkerman} ");
